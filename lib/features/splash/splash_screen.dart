2672002649e9dee49f23b91650e377e22b4f4598
shalabi11 <alshalabi311@gmail.com>
2026-03-25 07:35:07 +0100

    style: format splash screen layout

--- lib/features/splash/splash_screen.dart
+++ lib/features/splash/splash_screen.dart
@@ -12,14 +12,18 @@ class SplashScreen extends StatefulWidget {
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderStateMixin {
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
   late AnimationController _controller;
   late Animation<double> _animation;
 
   @override
   void initState() {
     super.initState();
-    _controller = AnimationController(vsync: this, duration: const Duration(seconds: 2));
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    );
     _animation = CurvedAnimation(parent: _controller, curve: Curves.easeInOut);
     _controller.forward();
 
@@ -54,7 +58,11 @@ class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderSt
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  const Icon(Icons.mosque_outlined, size: 100, color: AppColors.accent),
+                  const Icon(
+                    Icons.mosque_outlined,
+                    size: 100,
+                    color: AppColors.accent,
+                  ),
                   const SizedBox(height: 20),
                   Text(
                     AppStrings.appName.tr,
